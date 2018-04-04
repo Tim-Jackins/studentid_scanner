c13ebb6963b8e2b142390325a4191de6871dadb6
Tim-Jackins <timmins.jack595@gmail.com>
2018-04-04 12:22:27 -0400

    opencv to control webcam added

---
+++ software/scan_and_write.py
@@ -0,0 +1,77 @@
+import pytesseract
+import argparse
+import subprocess
+import os
+import re
+import cv2
+from PIL import Image, ImageEnhance, ImageFilter
+
+def get_image(camera, filename):
+	ramp_frames = 30 #This decides how much ramp the camera has to prepare
+	# Ramp the camera - these frames will be discarded and are only used to allow v4l2
+	# to adjust light levels, if necessary
+	print('Ramping camera...')
+    for i in range(ramp_frames):
+		temp = get_image(camera)
+	print('Taking image...')
+	camera_capture = get_image(camera)
+	del(camera)
+	img = cv2.cvtColor(camera_capture, cv2.COLOR_BGR2RGB)
+	im_pil = Image.fromarray(img)
+	return im_pil
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--transition", required=True, default='y', help='Are you y or n for entering?')
+args = vars(ap.parse_args())
+
+# Arguments
+dir_path = '.'
+transition = 'Entering' if args['transition'] == 'y' else 'Exiting'
+
+#picname = '{0}.jpg'.format(studentname)
+
+print('Scanning photo id...')
+#subprocess.call(['convert', picname, '-rotate', '270', picname]) #only rotate if you have to
+
+img = cv2.imread("path/to/img.png")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # You may need to convert the color
+im_pil = Image.fromarray(img)
+
+card = Image.open(picname)
+
+print('Processing photo...')
+card = card.filter(ImageFilter.MedianFilter())
+enhancer = ImageEnhance.Contrast(card)
+card = enhancer.enhance(2)
+card = card.convert('1')
+
+#name = card.crop((135, 940, 880, 1020))
+#studentid = card.crop((130, 1090, 590, 1150))
+
+card.save('cardadjust.jpg')
+subprocess.call(['mogrify', '-format', 'png', 'cardadjust.jpg'])
+os.system('rm *.jpg')
+os.system('mv cardadjust.png cut_up_card')
+os.chdir('cut_up_card')
+
+print('Reading photo...')
+cardText = str(pytesseract.image_to_string(Image.open('cardadjust.png')).encode('utf-8'))
+#try:
+index = cardText.index('b\'') + 2
+while cardText[index].isalpha() or cardText[index] == ' ': index += 1
+
+print('Name: {0}{1}Studentid: {2}{1}Transition: {3}'.format(
+	cardText[cardText.index('b\'') + 2: index], 
+	'\n', 
+	str(re.findall(r'\D(\d{9})\D', cardText)[0]),
+	transition))
+name = cardText[cardText.index('b\'') + 2: index]
+studentid = str(re.findall(r'\D(\d{9})\D', cardText)[0])
+
+os.chdir('..')
+print('Writing to spreadsheet')
+subprocess.call(['python3', 'write_to_sheet.py', '-n', name, '-i', studentid, '-t', transition])
+#except:
+#	print('Reading failed printing observed text:')
+#	print(cardText)
---
+++ software/take_photo.py
@@ -0,0 +1,52 @@
+import cv2
+
+def run_preview():
+	cv2.namedWindow("preview")
+	camera = cv2.VideoCapture(camera_port)
+	if camera.isOpened(): # try to get the first frame
+		rval, frame = camera.read()
+	else:
+		rval = False
+	while rval:
+		cv2.imshow("preview", frame)
+		rval, frame = camera.read()
+		key = cv2.waitKey(20)
+		if key == 27: # exit on ESC
+			break
+	cv2.destroyWindow("preview")
+ 
+# Captures a single image from the camera and returns it in PIL format
+def get_image(camera):
+	# read is the easiest way to get a full image out of a VideoCapture object.
+	retval, im = camera.read()
+	del(camera)
+	return im
+
+def take_image(camera, filename):
+	camera_capture = get_image(camera)
+	file = '/home/jtimmins/python/studentid_scanner/software/' + filename
+	cv2.imwrite(file, camera_capture)
+	del(camera)
+
+def take_good_image(camera, filename):
+	ramp_frames = 30
+	# Ramp the camera - these frames will be discarded and are only used to allow v4l2
+	# to adjust light levels, if necessary
+	for i in range(ramp_frames):
+		temp = get_image(camera)
+	print("Taking image...")
+	# Take the actual image we want to keep
+	camera_capture = get_image(camera)
+	file = '/home/jtimmins/python/studentid_scanner/software/' + filename
+	# A nice feature of the imwrite method is that it will automatically choose the
+	# correct format based on the file extension you provide. Convenient!
+	cv2.imwrite(file, camera_capture) 
+	# You'll want to release the camera, otherwise you won't be able to create a new
+	# capture object until your script exits
+	del(camera)
+
+if __name__ == '__main__':
+	camera_port = 1
+	camera = cv2.VideoCapture(camera_port)
+	take_image(camera, 'test_img_bad.png')
+	take_good_image(camera, 'test_img_good.png')
